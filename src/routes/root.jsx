73887c0823a294b1f8c227f19a6f552d348d4fe1
Luke Barker <luke.s.barker@gmail.com>
2022-12-07 16:42:21 -0600

    Cosmetic updates

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -2,7 +2,9 @@ export default function Root() {
   return (
     <>
       <div>
-        <h1>Wonky Words</h1>
+        <h1 className="flex flex-col text-center animate-bounce text-6xl  text-orange-400 ">
+          Wonky Words
+        </h1>
       </div>
     </>
   );
