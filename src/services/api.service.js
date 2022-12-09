00c0f56cc49acc06ab57cfface93b1aaae017ddb
Luke Barker <luke.s.barker@gmail.com>
2022-12-09 13:19:14 -0600

    Add json-server user and story fetches

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,10 +1,35 @@
-const BASE_URL = "https://www.dictionaryapi.com/api/v3/references/sd3/json/";
+const BASE_URL_DICTIONARY =
+  "https://www.dictionaryapi.com/api/v3/references/sd3/json/";
 const KEY = "c0d50870-63e8-4878-9257-8a4d8e590ab6";
+const BASE_URL_JSON_SERVER = "http://localhost:3001/";
 
 export default {
   async definition(query) {
-    const URL = BASE_URL + query + "?key=" + KEY;
+    const URL = BASE_URL_DICTIONARY + query + "?key=" + KEY;
+    const response = await fetch(URL);
+    return response.json();
+  },
 
-    return await fetch(URL);
+  async getAllUsers() {
+    const URL = BASE_URL_JSON_SERVER + "users";
+    const response = await fetch(URL);
+    return response.json();
+  },
+
+  async getUser(username, password) {
+    const URL =
+      BASE_URL_JSON_SERVER +
+      "users?username=" +
+      username +
+      "&password=" +
+      password;
+    const response = await fetch(URL);
+    return response.json();
+  },
+
+  async getAllStories() {
+    const URL = BASE_URL_JSON_SERVER + "stories";
+    const response = await fetch(URL);
+    return response.json();
   },
 };
