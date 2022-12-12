ea3baa8d5e41ad4a312457d9018a6b9f7644dbf4
Luke Barker <luke.s.barker@gmail.com>
2022-12-11 19:15:34 -0600

    Add About route

---
+++ src/routes/About.jsx
@@ -0,0 +1,10 @@
+export default function About() {
+
+  return (
+    <>
+      <h2>Welcome to Wonky Words</h2>
+      <p>This is a fun little site I created for my munchkins to play and learn.  The beautiful green and orange color scheme reflects each of their favorite colors.</p>
+
+    </>
+  );
+}
