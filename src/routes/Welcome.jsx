40c6d5f52156f9a82353fb515b201c13fd9de052
Luke Barker <luke.s.barker@gmail.com>
2022-12-14 11:51:38 -0600

    Add story list and links

--- src/routes/Welcome.jsx
+++ src/routes/Welcome.jsx
@@ -1,8 +1,23 @@
+import { useRouteLoaderData, Link } from "react-router-dom";
+
 export default function Welcome() {
+  const stories = useRouteLoaderData("app");
+
   return (
     <>
-      <h1>Wonky Words</h1>
-      <p>Welcome</p>
+      <div className="p-4 border-2 rounded-md border-orange-400 w-2/3 h-fit max-w-md">
+        <h2 className="font-bold text-xl h-fit text-center">
+          Here is a list of the Wonky Words stories that are available to choose
+          from:
+        </h2>
+        <ul className="mt-8 text-center text-xl font-medium">
+          {stories.map((story, index) => (
+            <li key={index}>
+              <Link to={`/story/` + story.id}> {story.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
