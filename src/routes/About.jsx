fdcf80617efb9797c9cc864a28b33ae8068b24f3
Luke Barker <luke.s.barker@gmail.com>
2022-12-14 11:54:55 -0600

    Update content and styles

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,10 +1,16 @@
 export default function About() {
-
   return (
     <>
-      <h2>Welcome to Wonky Words</h2>
-      <p>This is a fun little site I created for my munchkins to play and learn.  The beautiful green and orange color scheme reflects each of their favorite colors.</p>
-
+      <div>
+        <h2 className="font-bold text-xl h-fit text-center">
+          Welcome to Wonky Words
+        </h2>
+        <p className="p-4 text-center h-fit max-w-lg">
+          This is a fun little site I created for my munchkins to play and
+          learn. The beautiful green and orange color scheme reflects each of
+          their favorite colors.
+        </p>
+      </div>
     </>
   );
 }
