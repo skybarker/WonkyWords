7d5e135b4bf9e64d7d63cebed90d5b399ea95f3d
Luke Barker <luke.s.barker@gmail.com>
2022-12-14 11:53:14 -0600

    Add site links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 export default function Nav() {
   return (
-    <nav className="flex items-center gap-x-10 bg-green-400 py-2 mb-8">
+    <nav className="relative flex items-center gap-x-10 bg-green-400 py-2 px-4 mb-8 font-bold">
       <Link to={`/`}>Login</Link>
       <Link to={`/welcome`}>Stories</Link>
       <Link to={`/about`}>About</Link>
-      <div>Lookup widget</div>
-      <div>Welcome tag </div>
     </nav>
   );
 }
+
+Nav.propTypes = {
+  user: PropTypes.object,
+};
